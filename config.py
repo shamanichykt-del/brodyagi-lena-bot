f10c5869867e96012e28f08544cf1ae0a8e8c68e
shamanichykt-del <shamanichykt@icloud.com>
2026-07-17 22:36:49 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MAX_TOKEN = os.getenv("f9LHodD0cOLPws0g0KG7fKMyfSVsZms2j2rqHf1KpYv54gpUCiZI1HToNqLC2uapBHCFk4SRlg4Ag8YkccWi")
+OPENWEATHER_API_KEY = os.getenv("6501075f5080e33244a09deb62d201eb")
+CHANNEL_ID = os.getenv("https://max.ru/channel_brodyagy")
